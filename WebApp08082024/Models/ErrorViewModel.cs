cc5c13a2b7cbe365b9c206ad9c7e74f3b24b8e0a
ram goswami <Goswami@LAPTOP-Q1GKP9VJ.com>
2024-08-08 13:15:54 +0530

    Add project files.

---
+++ WebApp08082024/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApp08082024.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
